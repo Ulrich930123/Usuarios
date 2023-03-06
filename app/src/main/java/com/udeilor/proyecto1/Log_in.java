204f41ed79ea57ddd3618b0e00c94f6e78d321c0
Ulrich930123 <udeilor.uscategui@est.iudigital.edu.co>
2023-03-06 15:25:52 -0500

    splash para agregar

--- app/src/main/java/com/udeilor/proyecto1/Log_in.java
+++ app/src/main/java/com/udeilor/proyecto1/Log_in.java
@@ -7,17 +7,18 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Sign_Up extends AppCompatActivity {
+public class Log_in extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);
+        setContentView(R.layout.login);
         Button login =(Button) findViewById(R.id.login);
         Button signup=(Button) findViewById(R.id.signup);
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent l=new Intent(Sign_Up.this, Log_in.class);
+                Intent l=new Intent(Log_in.this, Log_in.class);
                 startActivity(l);
             }
 
@@ -25,10 +26,12 @@ public class Sign_Up extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View sign){
-                Intent s=new Intent(Sign_Up.this, Sign_Up.class);
+                Intent s=new Intent(Log_in.this, Sign_Up.class);
                 startActivity(s);
             }
 
         });
+
+
     }
 }
--- app/src/main/java/com/udeilor/proyecto1/MainActivity.java
+++ app/src/main/java/com/udeilor/proyecto1/MainActivity.java
@@ -7,31 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button login =(Button) findViewById(R.id.login);
-        Button signup=(Button) findViewById(R.id.signup);
-        login.setOnClickListener(new View.OnClickListener(){
+
+        TimerTask tarea=new TimerTask() {
             @Override
-            public void onClick(View v){
-                Intent l=new Intent(MainActivity.this, MainActivity.class);
-                startActivity(l);
+            public void run() {
+                Intent i=new Intent(MainActivity.this, Log_in.class);
+                startActivity(i);
+                finish();
+
             }
-
-        });
-        signup.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View sign){
-                Intent s=new Intent(MainActivity.this, Sign_Up.class);
-                startActivity(s);
-            }
-
-        });
-
+        };
+        Timer tiempo=new Timer();
+        tiempo.schedule(tarea,5000);
 
     }
 }
